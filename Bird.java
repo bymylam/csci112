a56a54e0940067e2458d5967e6c46d3951334440
My Lam <107911641+bymylam@users.noreply.github.com>
2024-03-11 11:46:51 -0400

    Delete Bird.java

--- Bird.java
+++
@@ -1,9 +0,0 @@
-public class Bird extends Pet implements CanFly {
-    //CONSTRUCTOR
-    public Bird(String name, int age) {
-        super(name, age);
-    }
-    public void fly() {
-        System.out.println(super.getName() + " flew around the room");
-    }
-}
